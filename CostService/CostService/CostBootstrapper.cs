b9b5ea06a76bf3d2bc5aa3f5bbfba77465defc4d
donnylu-iq <donnyl@iqmetrix.com>
2015-01-15 11:19:19 -0600

    created project

---
+++ CostService/CostService/CostBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace CostService
+{
+	public class CostBootstrapper : DefaultNancyBootstrapper	
+	{
+		protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+		{
+			base.ApplicationStartup(container, pipelines);
+		}
+	}
+}
---
+++ CostService/CostService/CostModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace CostService
+{
+	public class CostModule : NancyModule
+	{
+		public CostModule()
+		{
+			
+		}
+	}
+}
